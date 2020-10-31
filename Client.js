cefc7dfe6ce5125a6801b0550673d515ed29e3ca
William S. Takayama <60292132+william-takayama@users.noreply.github.com>
2020-10-31 15:31:33 -0300

    (Feat) Client class

---
+++ Client.js
@@ -0,0 +1,15 @@
+export class Client {
+  get cpf() {
+    return this._cpf;
+  }
+
+  constructor(name, cpf, password) {
+    this.name = name;
+    this._cpf = cpf;
+    this._password = password;
+  }
+
+  authenticate(password) {
+    return password == this._password;
+  }
+}
